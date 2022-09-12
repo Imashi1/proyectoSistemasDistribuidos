da8f777c6387b6c84d89560d9da90398fe8f4ae6
Tavitox-z <tavo.3900@gmail.com>
2022-09-12 10:52:08 -0300

    Usuario en chat arreglado

--- django-chat/chat/static/js/socketsmessages.js
+++ django-chat/chat/static/js/socketsmessages.js
@@ -1,17 +1,19 @@
 let url = `ws://${window.location.host}/ws/socket-server/`
-
+user = document.getElementById('username')
+console.log("usuario" + user.textContent);
 const chatSocket = new WebSocket(url)
 
 chatSocket.onmessage = function(e){
     let data = JSON.parse(e.data)
-    let user = document.getElementById('username')
+    
+
     console.log('Data:', data)
 
     if(data.type === 'chat'){
         let messages = document.getElementById('messages')
 
         messages.insertAdjacentHTML('beforeend', `<div>
-                                <p>${user.textContent}: ${data.message}</p>
+                                <p>${data.message}</p>
                             </div>`)
     }
 }
@@ -19,7 +21,7 @@ chatSocket.onmessage = function(e){
 let form = document.getElementById('form')
 form.addEventListener('submit', (e)=> {
     e.preventDefault()
-    let message = e.target.message.value 
+    let message = user.textContent + ":" + e.target.message.value 
     chatSocket.send(JSON.stringify({
         'message':message
     }))
